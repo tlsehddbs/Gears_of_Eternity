55471095d3a76d9d59ad68713dac416e87249b65
tlsehddbs <sdy4277@naver.com>
2025-04-28 01:22:25 +0900

    feat : Add Item effect enum script

---
+++ Gears_of_Eternity/Assets/Scripts/Enums/ItemBuffTypes.cs
@@ -0,0 +1,16 @@
+namespace ItemBuffTypes.Enums
+{
+    public enum ItemBuffType
+    {
+        AttackBuff,         // 공격력
+        DefenseBuff,        // 방어력 
+        MoveSpeedBuff,      // 이동속도
+        CriticalHitBuff,    // 치명타 확률
+        HealBuff,           // 체력 회복
+        CostChange,         // 소환 비용
+        CooldownReduction,  // 스킬 쿨타임
+        DamageAreaBuff,     // 피해 범위
+        DurationIncrease,   // 버프 지속 시간
+        EnemyDebuff         // 적 디버프
+    }
+}
