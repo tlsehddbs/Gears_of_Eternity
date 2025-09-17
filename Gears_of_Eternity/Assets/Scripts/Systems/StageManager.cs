9234ee140891c021a78572e1169d0ea472527e64
tlsehddbs <sdy4277@naver.com>
2025-09-17 16:48:38 +0900

    feat(stage): initialize stage-system with StageManager

---
+++ Gears_of_Eternity/Assets/Scripts/Systems/StageManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public static StageManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
